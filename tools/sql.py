29d1870960877db32256aeaa6bc89772c2d7ea05
Florian Hinterleitner <florian.hinterleitner@gmail.com>
2024-04-22 16:00:04 +0200

    tool added

--- tools/sql.py
+++ tools/sql.py
@@ -10,3 +10,10 @@ def run_sqlite_query(query):
     c = conn.cursor()
     c.execute(query)
     return c.fetchall()
+
+#create tool
+run_query_tool = Tool.from_function(
+    name = "run_sqlite_query",
+    description = "Run a sqlite query.",
+    func = run_sqlite_query
+)
